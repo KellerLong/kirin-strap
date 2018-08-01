ccbd1d2d5210840984c40307f0e992e5c83ead0e
kellerlong <kellerlong@163.com>
2018-08-01 16:30:47 +0800

    test

--- component.d.ts
+++ component.d.ts
@@ -2,7 +2,10 @@ import React from 'react';
 
 export default React;
 
-export declare function Cache(constructor: Function): Function;
-export declare function Component(constructor: Function): Function;
-export declare function ComponentLess(constructor: Function): Function;
-export declare function Route(constructor: Function): Function;
+export declare function Cache(constructor: Function): void;
+
+export declare function Component(constructor: Function): void;
+
+export declare function ComponentLess(constructor: Function): void;
+export declare function Model(constructor: Function): (a, b) => void;
+export declare function Route(path: string): (constructor: Function) => void;
--- src/component/index.ts
+++ src/component/index.ts
@@ -3,6 +3,8 @@ import cache from './Cache';
 import componentLess from './ComponentLess';
 import component from './Component';
 import route from './Route';
+import model from './Model';
+import state from './State';
 // component 部分入口
 
 // 组件
@@ -10,4 +12,6 @@ export const Cache = cache;
 export const ComponentLess = componentLess;
 export const Route = route;
 export const Component = component;
+export const Model = model;
+export const State = state;
 export default React;

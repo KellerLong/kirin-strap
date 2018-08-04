b1f906d711a3003459b62a938966326012509de8
kellerlong <kellerlong@163.com>
2018-08-04 10:34:54 +0800

    test

---
+++ model.d.ts
@@ -0,0 +1,5 @@
+export declare function Model(constructor: Function): void;
+export declare function Reducer(prototype, Name): void;
+export declare function Effect(prototype, Name): void;
+export declare function State(constructor: Function): (prototype, Name) => void;
+export declare function Subscripton(prototype, Name): void;

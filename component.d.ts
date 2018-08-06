9567dc67a30798c1971421ae4db7d834803ea01d
kellerlong <kellerlong@163.com>
2018-08-06 11:21:41 +0800

    test

--- component.d.ts
+++ component.d.ts
@@ -9,3 +9,7 @@ export declare function Component(constructor: Function): void;
 export declare function ComponentLess(constructor: Function): void;
 export declare function Model(modelPath: string): (a, b) => void;
 export declare function Route(path: string): (constructor: Function) => void;
+
+export declare  const Dva: any;
+export declare  const Router: any;
+export declare  const Dynamic: any;

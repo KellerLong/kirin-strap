7219f2bef19585234a60b649e64f8fe57157f67b
kellerlong <kellerlong@163.com>
2018-08-06 19:32:59 +0800

    test

--- network.js
+++ network.js
@@ -1,1 +1,149 @@
-module.exports=function(t){var e={};function n(r){if(e[r])return e[r].exports;var o=e[r]={i:r,l:!1,exports:{}};return t[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=t,n.c=e,n.d=function(t,e,r){n.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:r})},n.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},n.t=function(t,e){if(1&e&&(t=n(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var o in t)n.d(r,o,function(e){return t[e]}.bind(null,o));return r},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="./",n(n.s=0)}([function(t,e,n){t.exports=n(1)},function(t,e,n){"use strict";n.r(e);var r=function(t,e,n,r){return new(n||(n=Promise))(function(o,u){function i(t){try{f(r.next(t))}catch(t){u(t)}}function c(t){try{f(r.throw(t))}catch(t){u(t)}}function f(t){t.done?o(t.value):new n(function(e){e(t.value)}).then(i,c)}f((r=r.apply(t,e||[])).next())})},o=function(t,e){var n,r,o,u,i={label:0,sent:function(){if(1&o[0])throw o[1];return o[1]},trys:[],ops:[]};return u={next:c(0),throw:c(1),return:c(2)},"function"==typeof Symbol&&(u[Symbol.iterator]=function(){return this}),u;function c(u){return function(c){return function(u){if(n)throw new TypeError("Generator is already executing.");for(;i;)try{if(n=1,r&&(o=2&u[0]?r.return:u[0]?r.throw||((o=r.return)&&o.call(r),0):r.next)&&!(o=o.call(r,u[1])).done)return o;switch(r=0,o&&(u=[2&u[0],o.value]),u[0]){case 0:case 1:o=u;break;case 4:return i.label++,{value:u[1],done:!1};case 5:i.label++,r=u[1],u=[0];continue;case 7:u=i.ops.pop(),i.trys.pop();continue;default:if(!(o=(o=i.trys).length>0&&o[o.length-1])&&(6===u[0]||2===u[0])){i=0;continue}if(3===u[0]&&(!o||u[1]>o[0]&&u[1]<o[3])){i.label=u[1];break}if(6===u[0]&&i.label<o[1]){i.label=o[1],o=u;break}if(o&&i.label<o[2]){i.label=o[2],i.ops.push(u);break}o[2]&&i.ops.pop(),i.trys.pop();continue}u=e.call(t,i)}catch(t){u=[6,t],r=0}finally{n=o=0}if(5&u[0])throw u[1];return{value:u[0]?u[1]:void 0,done:!0}}([u,c])}}},u={url:"/",timeout:0,type:"GET",sendData:{},header:{"Content-Type":"text/html;charset=utf-8"}},i=function(){function t(){}return t.prototype.request=function(t){return void 0===t&&(t=u),r(this,void 0,void 0,function(){var e;return o(this,function(n){return e=t.type.toUpperCase(),{method:e},this.onNetworkRequestBefore(t,this.onNetworkRequestAppBefore)?(console.log("start request"),[2]):[2,!1]})})},t.prototype.onNetworkRequestBefore=function(t,e){return e()},t.prototype.onNetworkRequestAppBefore=function(t){return console.log("app start for network"),!0},t.prototype.getNetworkConfig=function(t){void 0===t&&(t=u)},t.post=function(t){return function(t,e){console.log("-----post")}},t.put=function(t,e,n){},t.get=function(t,e,n){},t.delete=function(t,e,n){},t.head=function(t,e,n){},t}();n.d(e,"Define",function(){return f}),n.d(e,"Service",function(){return l}),n.d(e,"Get",function(){return a}),n.d(e,"Post",function(){return p}),n.d(e,"Put",function(){return s}),n.d(e,"Delete",function(){return d}),n.d(e,"Head",function(){return y});var c=void 0,f=(e.default=i,function(){}),l=function(t){for(var e in t.prototype)t.prototype["__"+e+"__"]&&(t.prototype[e]=t.prototype["__"+e+"__"],delete t.prototype["__"+e+"__"])},a=i.get,p=function(t){return function(t,e){t["__"+e+"__"]=function(t){console.log("111111"),c.request()}}},s=i.put,d=i.delete,y=i.head}]);
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "./";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/network/Define.ts":
+/*!*******************************!*\
+  !*** ./src/network/Define.ts ***!
+  \*******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (function () { });\n\n\n//# sourceURL=webpack:///./src/network/Define.ts?");
+
+/***/ }),
+
+/***/ "./src/network/Network.ts":
+/*!********************************!*\
+  !*** ./src/network/Network.ts ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {\n    return new (P || (P = Promise))(function (resolve, reject) {\n        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }\n        function rejected(value) { try { step(generator[\"throw\"](value)); } catch (e) { reject(e); } }\n        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }\n        step((generator = generator.apply(thisArg, _arguments || [])).next());\n    });\n};\nvar __generator = (undefined && undefined.__generator) || function (thisArg, body) {\n    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;\n    return g = { next: verb(0), \"throw\": verb(1), \"return\": verb(2) }, typeof Symbol === \"function\" && (g[Symbol.iterator] = function() { return this; }), g;\n    function verb(n) { return function (v) { return step([n, v]); }; }\n    function step(op) {\n        if (f) throw new TypeError(\"Generator is already executing.\");\n        while (_) try {\n            if (f = 1, y && (t = op[0] & 2 ? y[\"return\"] : op[0] ? y[\"throw\"] || ((t = y[\"return\"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;\n            if (y = 0, t) op = [op[0] & 2, t.value];\n            switch (op[0]) {\n                case 0: case 1: t = op; break;\n                case 4: _.label++; return { value: op[1], done: false };\n                case 5: _.label++; y = op[1]; op = [0]; continue;\n                case 7: op = _.ops.pop(); _.trys.pop(); continue;\n                default:\n                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }\n                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }\n                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }\n                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }\n                    if (t[2]) _.ops.pop();\n                    _.trys.pop(); continue;\n            }\n            op = body.call(thisArg, _);\n        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }\n        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };\n    }\n};\n// import fetch from 'dva/fetch';\nvar OPTION_DEFAULT = {\n    url: '/',\n    timeout: 0,\n    type: 'GET',\n    sendData: {},\n    header: {\n        'Content-Type': 'text/html;charset=utf-8'\n    }\n};\nvar Network = /** @class */ (function () {\n    function Network() {\n    }\n    // 发起请求\n    Network.prototype.request = function (option) {\n        if (option === void 0) { option = OPTION_DEFAULT; }\n        return __awaiter(this, void 0, void 0, function () {\n            var method, fetchOption;\n            return __generator(this, function (_a) {\n                method = option.type.toUpperCase();\n                fetchOption = { method: method };\n                // 请求前\n                if (!this.onNetworkRequestBefore(option, this.onNetworkRequestAppBefore)) {\n                    return [2 /*return*/, false];\n                }\n                console.log('start request');\n                return [2 /*return*/];\n            });\n        });\n    };\n    Network.prototype.onNetworkRequestBefore = function (option, next) { return next(); };\n    Network.prototype.onNetworkRequestAppBefore = function (option) {\n        console.log('app start for network');\n        return true;\n    };\n    ;\n    Network.prototype.getNetworkConfig = function (option) {\n        if (option === void 0) { option = OPTION_DEFAULT; }\n    };\n    // 请求方式\n    Network.post = function (url) {\n        return function (a, b) {\n            console.log('-----post');\n        };\n    };\n    Network.put = function (url, sendData, network) { };\n    Network.get = function (url, sendData, network) { };\n    Network.delete = function (url, sendData, network) { };\n    Network.head = function (url, sendData, network) { };\n    return Network;\n}());\n/* harmony default export */ __webpack_exports__[\"default\"] = (Network);\n\n\n//# sourceURL=webpack:///./src/network/Network.ts?");
+
+/***/ }),
+
+/***/ "./src/network/Service.ts":
+/*!********************************!*\
+  !*** ./src/network/Service.ts ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (function (Service) {\n    for (var key in Service.prototype) {\n        if (Service.prototype[\"__\" + key + \"__\"]) {\n            Service.prototype[key] = Service.prototype[\"__\" + key + \"__\"];\n            delete Service.prototype[\"__\" + key + \"__\"];\n        }\n    }\n});\n\n\n//# sourceURL=webpack:///./src/network/Service.ts?");
+
+/***/ }),
+
+/***/ "./src/network/index.ts":
+/*!******************************!*\
+  !*** ./src/network/index.ts ***!
+  \******************************/
+/*! exports provided: default, Define, Service, Get, Post, Put, Delete, Head */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"Define\", function() { return Define; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"Service\", function() { return Service; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"Get\", function() { return Get; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"Post\", function() { return Post; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"Put\", function() { return Put; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"Delete\", function() { return Delete; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"Head\", function() { return Head; });\n/* harmony import */ var _Define__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Define */ \"./src/network/Define.ts\");\n/* harmony import */ var _Service__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Service */ \"./src/network/Service.ts\");\n/* harmony import */ var _Network__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Network */ \"./src/network/Network.ts\");\nvar _this = undefined;\n\n\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (_Network__WEBPACK_IMPORTED_MODULE_2__[\"default\"]);\nvar Define = _Define__WEBPACK_IMPORTED_MODULE_0__[\"default\"];\nvar Service = _Service__WEBPACK_IMPORTED_MODULE_1__[\"default\"];\nvar Get = _Network__WEBPACK_IMPORTED_MODULE_2__[\"default\"].get;\nvar Post = function (url) {\n    return function (service, name) {\n        service[\"__\" + name + \"__\"] = function (sendData) {\n            console.log('111111');\n            _this.request();\n        };\n    };\n};\nvar Put = _Network__WEBPACK_IMPORTED_MODULE_2__[\"default\"].put;\nvar Delete = _Network__WEBPACK_IMPORTED_MODULE_2__[\"default\"].delete;\nvar Head = _Network__WEBPACK_IMPORTED_MODULE_2__[\"default\"].head;\n\n\n//# sourceURL=webpack:///./src/network/index.ts?");
+
+/***/ }),
+
+/***/ 0:
+/*!************************************!*\
+  !*** multi ./src/network/index.ts ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__(/*! ./src/network/index.ts */\"./src/network/index.ts\");\n\n\n//# sourceURL=webpack:///multi_./src/network/index.ts?");
+
+/***/ })
+
+/******/ });

2e143270f7233db644a026cf95ddd10eb71d77c9
kellerlong <kellerlong@163.com>
2018-08-12 00:32:47 +0800

    test

--- network.d.ts
+++ network.d.ts
@@ -10,7 +10,7 @@ export declare function Service(constructor: Function): void;
 
 export default class Network {
   constructor();
-  onNetWorkRequestBefore(option: NetworkTyping.IOption, next);
-  onNetWorkResponseAfter(option: NetworkTyping.IOption, req, next);
-  onNetWorkRequestError(option: NetworkTyping.IOption, e: Error, next);
+  onNetworkRequestBefore(option: NetworkTyping.IOption, next);
+  onNetworkResponseAfter(option: NetworkTyping.IOption, req, next);
+  onNetworkRequestError(option: NetworkTyping.IOption, e: Error, next);
 }

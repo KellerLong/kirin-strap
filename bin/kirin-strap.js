14c15aff019da06667637c27df5b08a1402b0457
kellerlong <kellerlong@163.com>
2018-08-13 16:23:16 +0800

    test

--- bin/kirin-strap.js
+++ bin/kirin-strap.js
@@ -13,16 +13,23 @@ program
   .option('build-client,  build-client [type]', 'build to static debug or dev or build')
   .option('init [dir],  build-client [type]', 'build to static debug or dev or build')
   .option('build-server, build-server [type]', 'build to server debug or dev or build')
-  .option('--dev, buildDev [type]', 'build to server debug or dev or build')
-  .option('--debug, buildDebug [type]', 'build to server debug or dev or build')
-  .option('--prod, buildProd [type]', 'build to server debug or dev or build')
+  .option('--dev, dev [type]', 'build to server debug or dev or build')
+  .option('--debug, debug [type]', 'build to server debug or dev or build')
+  .option('--prod, prod [type]', 'build to server debug or dev or build')
   .parse(process.argv);
 // 初始化一个项目
 
 if (program.buildClient) {
   process.env.NODE_ENV = 'development';
+  process.env.BUILD_WAY = 'development';
+
   if (program.pord) {
+    process.env.BUILD_WAY = 'production';
     process.env.NODE_ENV = 'production';
+  }
+  if (program.debug) {
+    process.env.BUILD_WAY = 'debug';
+    process.env.NODE_ENV = 'development';
   }
 
   // 编译客户端
--- build/utils.js
+++ build/utils.js
@@ -1,239 +1,149 @@
-const webpack  = require("webpack");
-const path = require("path");
-const fs = require("fs");
-const express              = require("express");
-const WebpackDevMiddleware = require("webpack-dev-middleware");
-const WebpackHotMiddleware = require("webpack-hot-middleware");
-/**
- * some function of webpack use
- */
-class Util {
-  // 默认值配置
+const path = require('path');
+const webpack = require('webpack');
+const HtmlPlugin = require('html-webpack-plugin');
+const utils = require('./utils');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-  constructor() {
-    const YAML = require('yamljs');
-    this.XRenderOption = YAML.load(path.resolve('config/application.yml'));
-  }
+// const xOption = process.cwd();
+const NODE_ENV = process.env.NODE_ENV;
+const BUILD_WAY = process.env.BUILD_WAY;
+// 编译前做的事
+// 获取 yml 配置信息
+// 获取所有页面
+utils.gerRouterList();
 
-  /**
-   * build config
-   * @param webpackConfig
-   */
-  build ( webpackConfig ) {
-    // build
-    webpack( webpackConfig, ( error, stats ) => {
-      // error
-      if( error ) throw error;
-      // config of output style
-      const outputConfig        = {};
-      outputConfig.colors       = true;
-      outputConfig.modules      = false;
-      outputConfig.chunks       = false;
-      outputConfig.chunkModules = false;
-      // output stats
-      process.stdout.write( `${stats.toString( outputConfig )}\n` );
-      // exit node
-      process.exit();
-    });
-  }
+/****************************************************
+ * config entry
+ ***************************************************/
+const entry = {};
 
-  dev(webpackConfig) {
-    const env                   = process.env;
-    // set compiler build
-    const compiler              = webpack( webpackConfig );
-    // create server of express
-    const app                   = express();
+// ./src/index.ts
+entry.app = [`${__dirname}/router-list.tsx`];
 
-    const devConfig             = {};
-    devConfig.publicPath        = webpackConfig.output.publicPath;
-    devConfig.stats             = { colors : true };
-    devConfig.hot               = true;
-    devConfig.quiet             = false;
-    devConfig.noInfo            = false;
-    devConfig.lazy              = false;
-    // create hot middleware and dev middleware
-    const hotMiddleware         = WebpackHotMiddleware( compiler);
-    const devMiddleware         = WebpackDevMiddleware( compiler, devConfig );
-
-    // add event
-    compiler.plugin("compilation", ( compilation ) => {
-      compilation.plugin( "html-webpack-plugin-after-emit", ( data, cb ) => {
-        hotMiddleware.publish({ action : "reload" });
-        // cb();
-      });
-    });
-
-    // http use dev and hot loading
-    app.use( devMiddleware );
-    app.use( hotMiddleware );
-
-    // listen port
-    app.listen( env.PORT_DEVELOP, () => {
-      console.log( `Listening on ${env.PORT_DEVELOP}` );
-    });
-  }
-
-  /**
-   *
-   * @returns {{}}
-   */
-  getConfigHtmlTemplate () {
-    const option        = {}; // html
-    option.filename     = "index.html";
-    option.template     = "index.html";
-    return option;
-  }
-
-  /**
-   *
-   */
-  getConfigDefinePlugin () {
-    // const BUILD_TYPE        = process.env.BUILD_TYPE;
-    const NODE_ENV          = process.env.NODE_ENV;
-    const env               = {};
-
-    env.NODE_ENV            = `"${NODE_ENV}"`;
-    env.XRenderOption       =  JSON.stringify(this.XRenderOption);
-    // env.BUILD_TYPE          = `"${BUILD_TYPE}"`;
-
-    return { "process.env" : env };
-  }
-
-  /**
-   *
-   */
-  //getConfigCommons () {
-  //  const option            = {};
-  //  option.name             = "common";
-  //  option.filename         = `${process.env.DIRECTORY_OUTPUT_STATIC}/js/[name]-[hash:5].js`;
-  //  return option;
-  //}
-
-  getConfigOptimize () {
-    const option            = {};
-    option.compress         = { warnings: false };
-    option.sourceMap        = true;
-    return option;
-  }
-
-  getConfigPWAManifest() {
-    const option            = {};
-    const env               = process.env;
-    option.name = env.APP_NAME;
-    option.short_name = env.APP_SHORT_NAME;
-    option.description = env.APP_SHORT_DESCRIPTION;
-    option.background_color = env.APP_BACKGROUND_COLOR;
-    option.icons = [
-      {
-        src: path.resolve(env.APP_ICON),
-        sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
-      },
-      {
-        src: path.resolve(env.APP_ICON),
-        size: "1024x1024" // you can also use the specifications pattern
-      }
-    ];
-    return option;
-  }
-
-  getCssLoader() {
-    const env               = process.env;
-    return [
-      {
-        loader: 'css-loader',
-        options: {
-          minimize: 'production' === env.NODE_ENV
-        }
-      }
-    ];
-  }
-
-  getLessLoader() {
-    const env               = process.env;
-    //const cssLoader = [...this.getCssLoader()];
-    //cssLoader.push({
-    //  loader: 'less-loader'
-    //  });
-    return [
-      {
-        loader: 'css-loader',
-        options: {
-          modules: true,
-          localIdentName: "[name]-[local]-[hash:5]",
-          minimize: 'production' === env.NODE_ENV
-        }
-      },
-      {
-        loader: 'postcss-loader',
-        options: {
-          sourceMap: true,
-          config: {
-            path: '.postcssrc.js'
-          }
-        }
-      },
-      {
-        loader: "less-loader"
-      },
-      {
-        loader: "postcss-loader",
-        options: {
-          sourceMap: true,
-          plugins: [
-            require("stylelint")()
-          ]
-        }
-      }
-    ];
-  }
-
-  gerRouterList() {
-    const cwd = process.cwd();
-    // 获取所有page
-    const allPages = this.getFile(path.join(cwd, 'src', 'pages'), 'pages');
-    // 读取路由配置
-    const pageRouter = this.getRouterConfig(allPages);
-  }
-
-  getRouterConfig(pageList) {
-    let routerConfig = [];
-    const cwd = process.cwd();
-    pageList.map(pagePath => {
-      const filePath = path.join(cwd, 'src', pagePath);
-      // 读取文件
-      const code = fs.readFileSync(filePath, {encoding: 'utf-8'});
-
-      code.replace(/\@Route\((.*)\)/ig, (str, path) => {
-        const models = [];
-        code.replace(/\@Model\([\'|\"](.*)[\'|\"]\)/ig, (str, modelName) => {
-          models.push(`import('models/${modelName}.ts')`)
-        })
-        routerConfig.push(` {
-    path: ${path},
-    component: () => import('${pagePath}'),
-    models: [${models.join(',')}],
-  }`);
-      });
-
-      fs.writeFileSync(path.join(__dirname, 'router-config.ts'), `export default [\n${routerConfig.join(',\n')}\n];\n`);
-    });
-  }
-
-  getFile(pathFull, rootPath) {
-    const allFiles = fs.readdirSync(pathFull);
-    let retArray = [];
-    allFiles.map( filePath => {
-      const longPath = path.join(pathFull,filePath);
-      const stat = fs.statSync(longPath);
-      if (stat.isFile() && /\.tsx$/.test(filePath)) {
-        retArray.push(`${rootPath}/${filePath}`);
-      }
-      if (stat.isDirectory()) {
-        retArray = retArray.concat(this.getFile(longPath, `${rootPath}/${filePath}`));
-      }
-    });
-    return retArray;
-  }
+if ('development' === BUILD_WAY) {
+  entry.app = ['webpack-hot-middleware/client?reload=true'].concat(entry.app);
 }
 
-module.exports = new Util();
+/****************************************************
+ * config output
+ ***************************************************/
+const output = {};
+output.publicPath = './';
+output.path = path.join(process.cwd(), 'dist');
+output.filename = `static/js/[name]-[hash:5].js`;
+
+if ('development' === BUILD_WAY) {
+  output.path = '/';
+  output.publicPath = '/';
+}
+
+/****************************************************
+ * config resolve
+ ***************************************************/
+const resolve = {
+  extensions: [".ts", ".tsx",  ".less", ".js"],
+  alias: {
+    'src': path.resolve('src'),
+    'assets': path.resolve('src/assets'),
+    'entities': path.resolve('src/components'),
+    'pages': path.resolve('src/pages'),
+    'models': path.resolve('src/models'),
+    'styles': path.resolve('src/styles'),
+    'services': path.resolve('src/services'),
+  }
+};
+
+/****************************************************
+ * config module
+ ***************************************************/
+const stylePrivate = new ExtractTextPlugin(
+  {
+    filename: `static/styles/style-[hash:5].css`,
+    allChunks: true,
+  });
+const moduleOptions = {
+  rules: []
+};
+
+//typescript
+//----------------
+// ts
+moduleOptions.rules.push({
+  test: /\.(ts|tsx)$/,
+  loader: 'ts-loader',
+  options: {
+    allowTsInNodeModules: true
+  }
+});
+
+// tslint
+moduleOptions.rules.push({
+  test: /\.(ts|tsx)$/,
+  enforce: 'pre',
+  exclude: /node_modules/,
+  loader: 'tslint-loader',
+});
+
+// css
+moduleOptions.rules.push({
+  test: /\.css$/,
+  loader: stylePrivate.extract({
+    fallback:"style-loader",
+    use: utils.getCssLoader(),
+    publicPath: '/'
+  })
+});
+// less
+moduleOptions.rules.push({
+  test: /\.less$/,
+  loader: stylePrivate.extract({
+    fallback:"style-loader",
+    use: utils.getLessLoader(),
+    publicPath: '/'
+  })
+});
+// image
+moduleOptions.rules.push({
+  test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+  loader: 'url-loader',
+  query: {
+    limit: 1000,
+    name: `static/resources/[name]-[hash:5].[ext]`,
+    publicPath: '/'
+  }
+});
+
+// font
+moduleOptions.rules.push({
+  test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+  loader: 'url-loader',
+  query: {
+    limit: 1000,
+    name: `static/resources/[name]-[hash:5].[ext]`,
+    publicPath: '/'
+  }
+});
+
+/****************************************************
+ * config plugins
+ ***************************************************/
+const plugins = [stylePrivate];
+plugins.push(new webpack.DefinePlugin(utils.getConfigDefinePlugin()));
+plugins.push(new HtmlPlugin(utils.getConfigHtmlTemplate()));
+
+if ('development' === BUILD_WAY) {
+  plugins.push(new webpack.HotModuleReplacementPlugin());
+  plugins.push(new webpack.NoEmitOnErrorsPlugin());
+}
+/****************************************************
+ * config target
+ ***************************************************/
+
+let target = 'web';
+// if ('server' === env.BUILD_TYPE) {
+//   target = 'node';
+// }
+
+const mode = process.env.NODE_ENV;
+
+module.exports = {entry, mode, output, module: moduleOptions, resolve, plugins, target};
